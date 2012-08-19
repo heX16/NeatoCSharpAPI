a38d92b3cade81ee99441f8732b187084852aa51
Joel Ahlgren <erik.joel.ahlgren@gmail.com>
2012-08-19 21:57:52 +0200

    Fixed timing issues with sending commands and reading responses through SerialPort.

--- NeatoAPI/Connection.cs
+++ NeatoAPI/Connection.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using System.IO;
 using System.IO.Ports;
+using System.Threading;
 
 namespace Neato
 {
-    class Connection
+    /// <summary>
+    /// Represents the Serial Port connection to the Neato device.
+    /// </summary>
+    public class Connection
     {
+        #region Definitions and static methods.
+
         private SerialPort _port;
 
+        /// <summary>
+        /// Get a list of available COM ports on the current system.
+        /// </summary>
+        public static string[] GetPorts()
+        {
+            return SerialPort.GetPortNames();
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Creates a Connection object and attempts to connect to specified COM port.
+        /// </summary>
+        /// <param name="comPort">Name of port to connect to (eg. "COM3").</param>
         public Connection(string comPort)
         {
             // Values are inconsequential, quote from Programmers guide:
@@ -21,28 +42,63 @@ namespace Neato
             {
                 // TODO: Perform some kind of exception handling!
                 _port = null;
-                throw new Exception("Neato Connection Error!");
+                throw new Exception("Neato Connection Error!",e);
             }
         }
 
+        /// <summary>
+        /// Checks if connection to Neato is open.
+        /// </summary>
         public bool IsConnected()
         {
             return _port.IsOpen;
         }
-
+        
+        /// <summary>
+        /// Disconnects from Neato.
+        /// </summary>
         public void Disconnect()
         {
             _port.Close();
         }
 
+        /// <summary>
+        /// Sends a command to the Neato.
+        /// </summary>
+        /// <param name="command">Command (and any flags) to send to Neato.</param>
+        /// <returns>Response file with Neato response. Can be empty.</returns>
+        /// <exception cref="IOException">Thrown if no connection to Neato has been established.</exception>
         public Response SendCommand(string command)
         {
+            if(!IsConnected()) throw new IOException("No connection to Neato established.");
+
+            // Empty anything lingering in the buffer.
+            _port.DiscardInBuffer();
+
+            // Send command to Neato.
             _port.WriteLine(command);
 
-            //String tmp = _port.ReadTo("\u001F");
-
-            //return new Response(tmp);
-            return null;
+            // Wait a little bit for Neato's response.
+            Thread.Sleep(150);
+            string tmp = _port.ReadExisting();
+            
+            if(tmp.Length == 0)
+            {
+                // No response received from command.
+                return new Response("");
+            }
+            
+            // Cut off first line, contains command sent.
+            tmp = tmp.Substring(tmp.IndexOf('\n'));
+            
+            if(tmp.Contains("\n")) // Checks if there are any new lines.
+            {
+                // Cut off last line, it will only contain Ctrl+Z control character.
+                tmp = tmp.Substring(0, tmp.LastIndexOf('\n'));
+            }
+            
+            
+            return new Response(tmp);
         }
     }
 }
